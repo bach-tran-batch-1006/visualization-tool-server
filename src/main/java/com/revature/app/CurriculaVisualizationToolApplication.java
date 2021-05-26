1a0b9003c7c8b3e567457c2aae0b3e3c7960fd06
hpatel183 <hmpatel183@outlook.com>
2021-05-26 15:56:04 -0500

    Add unit test addCurriculum

--- src/main/java/com/revature/app/CurriculaVisualizationToolApplication.java
+++ src/main/java/com/revature/app/CurriculaVisualizationToolApplication.java
@@ -3,7 +3,12 @@ package com.revature.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@ComponentScan("com.revature.app")
+@EnableJpaRepositories("com.revature.app.dao")
 @SpringBootApplication
 @EntityScan("com.revature.app.model")
 public class CurriculaVisualizationToolApplication {
