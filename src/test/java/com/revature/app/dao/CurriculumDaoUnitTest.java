2775aa3b65fd4aa1fed64d8b31b17cf149ebc451
Joshua Hulbert <joshua.hulbert@revature.net>
2021-05-26 10:53:52 -0400

    Skeleton initialization of addCurriculumTest

--- src/test/java/com/revature/app/dao/CurriculumDaoUnitTest.java
+++ src/test/java/com/revature/app/dao/CurriculumDaoUnitTest.java
@@ -1,5 +1,26 @@
 package com.revature.app.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class CurriculumDaoUnitTest {
 
+	@Test
+	void simpleTest() {
+		assertEquals(1,1);
+	}
+	
+	
+	@Test
+	@Disabled
+	void addCurriculumTest() {
+		
+	}
 }
