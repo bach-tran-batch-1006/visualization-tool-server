484c540b7321fa89b9815b720aaa29535dec6669
Biren Sarvaiya <birensarvaiya24@gmail.com>
2021-05-26 12:57:51 -0400

    fix main class imports

--- src/main/java/com/revature/app/CurriculaVisualizationToolApplication.java
+++ src/main/java/com/revature/app/CurriculaVisualizationToolApplication.java
@@ -3,8 +3,6 @@ package com.revature.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.revature.app.model")
