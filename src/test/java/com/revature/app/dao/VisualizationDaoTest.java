4fece9b4e1a3a0563241cf2513b1bacfcc5413d9
Zuberi Reid <windbird44@gmail.com>
2021-05-27 14:08:27 -0400

    update data layer tests

--- src/test/java/com/revature/app/dao/VisualizationDaoTest.java
+++ src/test/java/com/revature/app/dao/VisualizationDaoTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.MethodOrderer;
@@ -18,6 +19,7 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.revature.app.model.Curriculum;
 import com.revature.app.model.Visualization;
 
 @ExtendWith(SpringExtension.class)
@@ -38,17 +40,21 @@ public class VisualizationDaoTest {
 
 		assertEquals(0, list.size());
 	}
-	
-	//creates
+
+	// creates
 
 	@Test
 	@Commit
 	@Order(10)
 	void testCreateVisualization() {
 		String payload = "test";
+		List<Curriculum> list = new ArrayList<>();
+		
 		Visualization expected = new Visualization();
-
+		
+		
 		expected.setVisualizationName(payload);
+		expected.setCurriculumList(list);
 
 		Visualization actual = visualDao.save(expected);
 
@@ -64,56 +70,61 @@ public class VisualizationDaoTest {
 
 		assertNotEquals(0, list.size());
 	}
-	
-	//reads
-	
+
+	// reads
+
 	@Test
 	@Order(20)
 	void testGetValidVisualization() {
 		Visualization actual = visualDao.getById(1);
-		
+
 		assertNotNull(actual);
 		assertEquals(1, actual.getVisualizationId());
 	}
-	
+
 	@Test
 	@Order(21)
 	void testGetInvalidVisualization() {
 		try {
-			 Visualization v = visualDao.getById(Integer.MAX_VALUE);
-			 System.out.println(v);
-			 fail("Exception not caught");
+			Visualization v = visualDao.getById(Integer.MAX_VALUE);
+			System.out.println(v);
+			fail("Exception not caught");
 		} catch (javax.persistence.EntityNotFoundException e) {
 			assertEquals("Unable to find com.revature.app.model.Visualization with id 2147483647", e.getMessage());
 		}
 	}
-	
-	//updates
-	
+
+	// updates
+
 	@Test
+	@Commit
 	@Order(30)
 	void testUpdateValidVisualization() {
 		String payload = "new name";
 		Visualization actual = visualDao.getById(1);
 		actual.setVisualizationName(payload);
-		Visualization newActual = visualDao.save(actual);
-		
-		assertNotNull(newActual);
-		assertEquals(payload, newActual.getVisualizationName());
+		Visualization expected = visualDao.save(actual);
+
+		assertNotNull(expected);
+		assertEquals(payload, expected.getVisualizationName());
 	}
-	
-	//deletes
+
+	// deletes
 	@Test
+	@Commit
 	@Order(40)
 	void testDeleteValidVisualization() {
 		visualDao.deleteById(1);
 	}
-	
+
 	@Test
+	@Commit
 	@Order(41)
 	void testDeleteInvalidVisualization() {
 		try {
-			visualDao.getById(Integer.MAX_VALUE);
+			Visualization v = visualDao.getById(Integer.MAX_VALUE);
+			 System.out.println(v);
+			 fail("Exception not caught");
 		} catch (javax.persistence.EntityNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
