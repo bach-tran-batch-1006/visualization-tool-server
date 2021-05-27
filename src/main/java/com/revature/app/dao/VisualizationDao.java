168dc8f1baae1729d0784c49749ee40cfaa039d7
Zuberi Reid <windbird44@gmail.com>
2021-05-27 02:55:55 -0400

    data layer in progress

--- src/main/java/com/revature/app/dao/VisualizationDao.java
+++ src/main/java/com/revature/app/dao/VisualizationDao.java
@@ -1,5 +1,7 @@
 package com.revature.app.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,16 @@ import com.revature.app.model.Visualization;
 
 @Repository
 public interface VisualizationDao extends JpaRepository<Visualization, Integer> {
-
+	
+	
+	//READ
+	public Visualization getVisualizationByID(int visualizationID);
+	
+	public List<Visualization> findAll();
+	
+	//UPDATE
+	public Visualization updateVisualization(int visualizationID, String payload);
+	
+	//DELETE
+	public boolean deleteVisualization(int visualizationID);
 }
--- src/test/java/com/revature/app/dao/VisualizationDaoTest.java
+++ src/test/java/com/revature/app/dao/VisualizationDaoTest.java
@@ -1,5 +1,34 @@
 package com.revature.app.dao;
 
-public class VisualizationDaoTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.revature.app.model.Visualization;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class VisualizationDaoTest {
+	@Autowired 
+	private VisualizationDao visualizationDao;
+	
+	@Test
+	@Order(0)
+	void testVisualizationListEmptu() {
+		List<Visualization> list = visualizationDao.findAll();
+		
+		assertEquals(0, list.size());
+	}
 }
