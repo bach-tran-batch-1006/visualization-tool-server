e413b01518c92bd3b9e7d4fbb2e96e478f58821d
hpatel183 <hmpatel183@outlook.com>
2021-05-30 16:08:31 -0500

    Add Controller Unit Test

--- src/test/java/com/revature/app/service/CurriculumServiceUnitTest.java
+++ src/test/java/com/revature/app/service/CurriculumServiceUnitTest.java
@@ -81,7 +81,7 @@ class CurriculumServiceUnitTest {
 	void test_getCurriculum_Idnotexist() throws CurriculumNotFoundException {
 		try {
 			new CurriculumDto("Language", null);
-			curriculumService.getCurriculumByID(0);
+			curriculumService.getCurriculumByID(10);
 			
 		} catch (CurriculumNotFoundException e) {
 			assertEquals(e.getMessage(), "Curriculum not found");
