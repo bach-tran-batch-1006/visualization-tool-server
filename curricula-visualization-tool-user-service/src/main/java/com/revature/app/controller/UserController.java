f069271b50aca7dc6e34a764d87d7ccc3c941c2f
Laith <40428921+laithalaboodi@users.noreply.github.com>
2021-08-25 09:34:22 -0700

    changed port to 8085 and tested signup/login and works

---
+++ curricula-visualization-tool-user-service/src/main/java/com/revature/app/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.revature.app.controller;
+
+import java.util.LinkedHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.app.model.User;
+import com.revature.app.service.UserService;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@RestController
+@RequestMapping(value="/user")
+@AllArgsConstructor(onConstructor=@__(@Autowired))
+@NoArgsConstructor
+@CrossOrigin(origins="*")
+public class UserController {
+	
+	private UserService uServ;
+	
+	@PostMapping("/register")
+	public ResponseEntity<User> createUser(@RequestBody LinkedHashMap<String, String> user){
+		System.out.println(user);
+		User u = new User(user.get("first"), user.get("last"), user.get("email"), user.get("pass"));
+		
+		u = uServ.registerUser(u);
+		
+		if(u != null) {
+			
+			return new ResponseEntity<User>(u, HttpStatus.CREATED);
+		}
+		else {
+			return new ResponseEntity<User>(u, HttpStatus.CONFLICT);
+		}
+	}//end of regester 
+	
+	@PostMapping("/login")
+	public ResponseEntity<User> loginUser(@RequestBody LinkedHashMap<String, String> user){
+		User u = uServ.loginUser(user.get("email"), user.get("pass"));
+		
+		
+		if(u == null) {
+			return new ResponseEntity<User>(u, HttpStatus.FORBIDDEN);
+		}
+
+		//triggerMail(u.getEmail(),"Dear "+u.getFirst()+" "+u.getLast()+",\n"+"logged in at this time: "+dtf.format(now)+"\nif it was not you please update your password: @ "+updateInfoLink,"Time Store Security login");
+		return new ResponseEntity<User>(u, HttpStatus.OK);
+	}
+
+}
---
+++ curricula-visualization-tool-user-service/src/main/java/com/revature/app/service/UserService.java
@@ -0,0 +1,65 @@
+package com.revature.app.service;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.app.model.User;
+
+import com.revature.app.repos.UserRepo;
+
+import lombok.NoArgsConstructor;
+
+import lombok.AllArgsConstructor;
+
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor(onConstructor=@__(@Autowired))
+public class UserService {
+	
+	private UserRepo uDao;
+	
+	public User registerUser(User u) {
+		try {
+			return uDao.save(u);
+		} catch(Exception e) {
+			return null;
+		}
+	}
+	
+public User loginUser(String email, String password) {
+		//findByUsername will return null if the user does not exist
+		User u = uDao.findByEmail(email);
+		//If username does not exist return null
+		if(u == null) {
+			return null;
+		}
+		else {
+			//If you user exists but password is wrong return null
+			if(!u.getPass().equals(password)) {
+				return null;
+			}
+			//Else return the logged in user
+			else {
+				return u;
+			}
+		}
+	}
+
+public User displayUser(String email) {
+	User u = uDao.findByEmail(email);
+		if(u == null) {
+			return null;
+		}
+		else {
+			return u;
+		}
+	}
+
+	public User getUserById(int id) {
+		return uDao.getById(id);
+	}
+}
