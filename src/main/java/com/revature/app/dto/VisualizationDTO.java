c257a2d63ea16d9ab47cfc0922037b1150d689b7
Scionoftruth <charlieallen132@hotmail.com>
2021-08-26 13:50:04 -0500

    Revert "save visualizationworking"
    This reverts commit caff30259768281c9f46f1ce9c808f9374363533.

--- src/main/java/com/revature/app/dto/VisualizationDTO.java
+++ src/main/java/com/revature/app/dto/VisualizationDTO.java
@@ -1,29 +1,20 @@
 package com.revature.app.dto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.app.model.Curriculum;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Data @Getter @Setter @ToString @EqualsAndHashCode @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @ToString @EqualsAndHashCode @AllArgsConstructor @NoArgsConstructor
 public class VisualizationDTO {
 
 	String title;
-	int UserId;
+	
 	List<Curriculum> curricula;
-	
-	public VisualizationDTO(String title, int UserId) {
-		this.title = title;
-		this.UserId = UserId;
-		this.curricula = new ArrayList<>();
-	}
-	
 }
