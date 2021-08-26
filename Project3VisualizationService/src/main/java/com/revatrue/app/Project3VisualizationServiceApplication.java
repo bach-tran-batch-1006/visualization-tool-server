7c2d74fa4221132984890e7f6fc4e3bf6d26c4a4
DanielRatliff <77987068+DanielRatliff@users.noreply.github.com>
2021-08-25 20:29:00 -0500

    add non working visualization part of microservice
    -this is what i have end of 8/25/21
    -testing and working logic to come

---
+++ Project3VisualizationService/src/main/java/com/revatrue/app/Project3VisualizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revatrue.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3VisualizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3VisualizationServiceApplication.class, args);
+	}
+
+}
---
+++ Project3VisualizationService/src/test/java/com/revatrue/app/Project3VisualizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revatrue.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3VisualizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
