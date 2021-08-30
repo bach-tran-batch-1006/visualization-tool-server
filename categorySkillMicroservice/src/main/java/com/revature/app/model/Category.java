aba7fa6b253caa39e01846cfa58bedac86eaf664
Andy <hient766@gmail.com>
2021-08-30 14:28:36 -0700

    adding category/skill microservice

---
+++ categorySkillMicroservice/src/main/java/com/revature/app/model/Category.java
@@ -0,0 +1,67 @@
+package com.revature.app.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.PreRemove;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name="Categories")
+@AllArgsConstructor @EqualsAndHashCode @Getter @Setter @ToString @NoArgsConstructor
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private int categoryId;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+	
+	@Column(name = "category_description")
+	private String categoryDescription;
+
+	@OneToMany(cascade = CascadeType.PERSIST, mappedBy="category") 
+	@JsonIgnore
+	private List<Skill> SkillList = new ArrayList<Skill>();
+	
+	public Category(String categoryName, String categoryDescription) {
+		super();
+		this.categoryName = categoryName;
+		this.categoryDescription = categoryDescription;
+	}
+
+	public Category(int categoryId, String categoryName, String categoryDescription) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+		this.categoryDescription = categoryDescription;
+	}
+	
+	@PreRemove
+	private void preRemove() {
+	    for (Skill s : SkillList ) {
+	        s.setCategory(null);
+	    }
+	}
+	
+}
---
+++ categorySkillMicroservice/src/main/java/com/revature/app/service/CategoryService.java
@@ -0,0 +1,126 @@
+package com.revature.app.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.*;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.revature.app.dao.CategoryDAO;
+
+import com.revature.app.dto.CategoryDTO;
+import com.revature.app.exception.BadParameterException;
+import com.revature.app.exception.CategoryBlankInputException;
+import com.revature.app.exception.CategoryNotFoundException;
+import com.revature.app.exception.EmptyParameterException;
+import com.revature.app.exception.ForeignKeyConstraintException;
+import com.revature.app.model.Category;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor(onConstructor= @__(@Autowired))
+@Getter @Setter @ToString 
+public class CategoryService {
+
+
+	private CategoryDAO categoryDAO;
+	
+
+
+
+	@Transactional
+	public Category addCategory(CategoryDTO inputCategory) throws CategoryBlankInputException {
+		Category category = null;
+		if(inputCategory.getCategoryName().trim().equals("")) {
+			throw new CategoryBlankInputException("The category name was left blank");
+		}
+		category = new Category(0, inputCategory.getCategoryName(), inputCategory.getCategoryDescription());
+		category = categoryDAO.save(category);
+		return category;
+	}
+
+	@Transactional
+	public List<Category> getAllCategories() {
+		return categoryDAO.findAll();
+
+	}
+
+	
+	
+
+	@Transactional
+	public Category findCategory(int id) {
+		
+		Category carte = null;
+		
+		carte = categoryDAO.findById(id);
+		if ( carte!= null) {
+			
+			return carte;
+		}
+		return carte;
+	}
+	
+	@Transactional
+	public Category updateCategory(String catId, CategoryDTO inputCategory) throws BadParameterException, CategoryNotFoundException, EmptyParameterException {
+		Category categoryToUpdate = null;
+		try {
+			if(catId.trim().equals("")){
+				throw new EmptyParameterException("The category ID was left blank");
+			}
+			if(inputCategory.getCategoryName().trim().equals("")){
+				throw new EmptyParameterException("The category name was left blank");
+			}
+			int id = Integer.parseInt(catId);
+			categoryToUpdate = categoryDAO.findById(id);
+			if(categoryToUpdate == null) {
+				throw new CategoryNotFoundException("The category could not be updated because it couldn't be found");
+			} else {
+				categoryToUpdate.setCategoryName(inputCategory.getCategoryName());
+				categoryToUpdate.setCategoryDescription(inputCategory.getCategoryDescription());
+				categoryToUpdate = categoryDAO.save(categoryToUpdate);
+			}
+			return categoryToUpdate;
+		} catch (NumberFormatException e) {
+			throw new BadParameterException("The category ID provided must be of type int");
+		}
+	}
+	
+	@Transactional(rollbackOn = {CategoryNotFoundException.class, ForeignKeyConstraintException.class})
+	public Category deleteCategory(String catId) throws EmptyParameterException, CategoryNotFoundException, BadParameterException, ForeignKeyConstraintException {
+		Category categoryToDelete = null;
+		try {
+			if(catId.trim().equals("")){
+				throw new EmptyParameterException("The category ID was left blank");
+			}
+			int id = Integer.parseInt(catId);
+			categoryToDelete = categoryDAO.findById(id);
+			if(categoryToDelete == null) {
+				throw new CategoryNotFoundException("The skill could not be deleted because it couldn't be found");
+			} else {
+				
+				
+				categoryDAO.delete(categoryToDelete);
+				categoryDAO.flush();
+			}
+			return categoryToDelete;
+		} catch (NumberFormatException e) {
+			throw new BadParameterException("The category ID provided must be of type int");
+		} catch (DataIntegrityViolationException e) {
+			throw new ForeignKeyConstraintException("Please remove this category from all skills before attempting to delete this category");
+		}
+	}
+
+
+
+}
