e7dd17fce808458da0018452d31add8366750b66
Scionoftruth <charlieallen132@hotmail.com>
2021-09-06 20:14:26 -0500

    Update Project3VisualizationServiceApplicationTests.java

--- Project3VisualizationService/src/test/java/com/revature/app/Project3VisualizationServiceApplicationTests.java
+++ Project3VisualizationService/src/test/java/com/revature/app/Project3VisualizationServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.revature.app;
 
-import org.junit.jupiter.api.Test;
+/*import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest*/
 class Project3VisualizationServiceApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
